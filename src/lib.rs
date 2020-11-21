9eff287fb6030f1e318b15b29846fd2a0c439786
knokko <knokogator@hotmail.com>
2020-11-21 12:28:51 +0100

    Silence a warning

--- src/lib.rs
+++ src/lib.rs
@@ -20,5 +20,7 @@ pub use shader::*;
 pub use util::*;
 pub use vertex::*;
 
+// This import generates an 'unused import' warning, but the unit tests won't compile without it.
+#[allow(unused_imports)]
 #[macro_use]
 extern crate lazy_static;
